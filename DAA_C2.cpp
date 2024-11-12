ed6a58335703356a57eae74f65a529b9a652b365
Gautam Santosh Kabra <151127377+gautam-kabra@users.noreply.github.com>
2024-11-12 13:30:30 +0530

    Update DAA_C2.cpp

--- DAA_C2.cpp
+++ DAA_C2.cpp
@@ -1,4 +1,12 @@
-// C++ code to implement the approach
+/*Implement Subset Sum Problem.
+Statement Given a set of non-negative integers and a
+value sum, the task is to check if there is a subset of the
+given set whose sum is equal to the given sum.
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
+Output: False
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
