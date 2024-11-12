3892541aa36f048c80fe42d7ed9380f04bc54228
Gautam Santosh Kabra <151127377+gautam-kabra@users.noreply.github.com>
2024-11-12 13:27:17 +0530

    Update DAA_A3.cpp

--- DAA_A3.cpp
+++ DAA_A3.cpp
@@ -1,3 +1,11 @@
+/*Implement a problem of smallest number with at
+least n trailing zeroes in factorial.
+Statement: Given a number n. The task is to find the
+smallest number whose factorial contains at least n
+trailing zeroes.
+Input : n = 1 Output : 5
+Input : n = 6 Output : 25
+*/
 #include <iostream>
 using namespace std;
 
