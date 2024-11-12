7bd4aee01c377b82cf7f109688b09fc01a92ec63
Gautam Santosh Kabra <151127377+gautam-kabra@users.noreply.github.com>
2024-11-12 13:29:00 +0530

    Update DAA_B2.cpp

--- DAA_B2.cpp
+++ DAA_B2.cpp
@@ -1,4 +1,13 @@
-// C++ program for the above approach
+/*Implement a problem of maximize Profit by trading
+stocks based on given rate per day.
+Statement: Given an array arr[] of N positive integers
+which denotes the cost of selling and buying a stock on
+each of the N days. The task is to find the maximum
+profit that can be earned by buying a stock on or selling
+all previously bought stocks on a particular day.
+Input: arr[] = {2, 3, 5} Output: 5
+Input: arr[] = {8, 5, 1} Output: 0
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
