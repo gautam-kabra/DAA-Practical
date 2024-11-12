2f43798bda33cde6d60680f0f19a275e6a88cfb6
Gautam Santosh Kabra <151127377+gautam-kabra@users.noreply.github.com>
2024-11-12 13:31:34 +0530

    Update DAA_D1.cpp

--- DAA_D1.cpp
+++ DAA_D1.cpp
@@ -1,6 +1,13 @@
+/*Implement program to find all distinct subsets of a
+given set using Bit Masking Approach.
+Statement Given an array of integers arr[], The task is
+to find all its subsets. The subset cannot contain duplicate elements, so any repeated subset should be
+considered only once in the output.
+Input: S = {1, 2, 2} Output: {}, {1}, {2}, {1, 2}, {2,
+2}, {1, 2,2}
+Input: S = {1, 2} Output: {}, {1}, {2}, {1, 2}
+*/
    
-// C++ program to find all subsets of given set. Any 
-// repeated subset is considered only once in the output 
 #include <iostream> 
 #include <vector> 
 #include <algorithm> 
