a2ad241f7e5cb76b607a0fadc1d6b46a3c41bef4
Gautam Santosh Kabra <151127377+gautam-kabra@users.noreply.github.com>
2024-11-12 13:29:36 +0530

    Update DAA_B3.cpp

--- DAA_B3.cpp
+++ DAA_B3.cpp
@@ -1,4 +1,16 @@
-// C++ code to implement the approach
+/*Implement a problem of minimum work to be done
+per day to finish given tasks within D days problem.
+Statement: Given an array task[] of size N denoting
+amount of work to be done for each task, the problem
+is to find the minimum amount of work to be done on
+each day so that all the tasks can be completed in at
+most D days. Note: On one day work can be done for
+only one task.
+Input: task[] = [3, 4, 7, 15], D = 10
+Output: 4
+Input: task[] = [30, 20, 22, 4, 21], D = 6
+Output: 22
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
