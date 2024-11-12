087977df22ac65acd5e00e15621aea04c7d91164
Gautam Santosh Kabra <151127377+gautam-kabra@users.noreply.github.com>
2024-11-12 13:26:07 +0530

    Update DAA_A1.cpp

--- DAA_A1.cpp
+++ DAA_A1.cpp
@@ -1,4 +1,9 @@
-// number of zeros at the end using binary search
+/*Implement a problem of number of zeroes.
+Statement: Given an array of 1s and 0s which has all
+1s first followed by all 0s? Find the number of 0s.
+Count the number of zeroes in the given array.
+Input: arr[] = {1, 1, 1, 1, 0, 0} Output: 2
+Input: arr[] = {1, 0, 0, 0, 0} Output: 4*/
 #include <bits/stdc++.h>
 using namespace std;
 
